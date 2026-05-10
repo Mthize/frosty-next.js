3c0497c581f48acbb56df016f7e1f45939dd76ea
Mthize <trestle_pistons0p@icloud.com>
2026-05-10 20:18:35 +0200

    Simplify Bounded component props typing

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,29 +1,25 @@
-import type { JSX } from "react";
 import clsx from "clsx";
 
-type BoundedProps<T extends keyof JSX.IntrinsicElements = "section"> =
-  JSX.IntrinsicElements[T] & {
-    as?: T;
-    className?: string;
-    children?: React.ReactNode;
-  };
+type BoundedProps = {
+  as?: string;
+  className?: string;
+  children?: React.ReactNode;
+};
 
-export const Bounded = <T extends keyof JSX.IntrinsicElements = "section">({
-  as: Comp,
+export const Bounded = ({
+  as: Comp = "section",
   className,
   children,
   ...restProps
-}: BoundedProps<T>) => {
-  const Tag = (Comp ?? "section") as keyof JSX.IntrinsicElements;
-
+}: BoundedProps) => {
   return (
-    <Tag
+    <Comp
       className={clsx("px-4 first:pt-10 md:px-6", className)}
       {...restProps}
     >
       <div className="mx-auto flex w-full max-w-7xl flex-col items-center">
         {children}
       </div>
-    </Tag>
+    </Comp>
   );
 };
